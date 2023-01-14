d942910535208fddda9f45b69f43d7c02400787d
Rene Santiago <rsj510@gmail.com>
2023-01-13 20:58:52 -0500

    added model and middleware

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const { PORT = 3000, DATABASE_URL } = process.env
 const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
+const cors = require("cors")
+const morgan = require("morgan")
 
 // Database Connection
 mongoose.set('strictQuery', false)
@@ -16,9 +18,23 @@ mongoose.connection
     .on("close", () => console.log("You are disconnected from mongoose"))
     .on("error", (error) => console.log(error))
 
+// Models
+const BookmarkSchema = new mongoose.Schema({
+    title: String,
+    url: String
+})
+
+const Bookmark = mongoose.model("Bookmark", BookmarkSchema)
+
+// Middleware
+app.use(cors())
+app.use(morgan("dev"))
+app.use(express.json())
+
 // Routes
 app.get("/", (req,res) => {
     res.send("hello world")
 })
 
+// Listener
 app.listen(PORT, () => console.log(`You are now on Port: ${PORT}`))

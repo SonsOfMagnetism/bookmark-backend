2371a77cb4367de6cfe404356d73b8803de05b7d
Cindy Wong <cindywongdev@gmail.com>
2023-01-14 20:27:30 -0500

    removed connection code, changed mongoose import to import the connection.js file

--- server.js
+++ server.js
@@ -3,20 +3,9 @@ require("dotenv").config()
 const { PORT = 3000, DATABASE_URL } = process.env
 const express = require("express")
 const app = express()
-const mongoose = require("mongoose")
+const mongoose = require("./models/connection")
 const cors = require("cors")
 const morgan = require("morgan")
-
-// Database Connection
-mongoose.set('strictQuery', false)
-mongoose.connect(DATABASE_URL, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-})
-mongoose.connection
-    .on("open", () => console.log("You are connected to mongoose"))
-    .on("close", () => console.log("You are disconnected from mongoose"))
-    .on("error", (error) => console.log(error))
 
 // Models
 const BookmarkSchema = new mongoose.Schema({
